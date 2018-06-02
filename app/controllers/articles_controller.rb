1c6c3878f556f606bf9540c16d83fca33cd85ac2
ImperatorNero <combe.g.f@gmail.com>
2018-06-02 16:27:55 +0200

    Delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
-
   def index
-   @articles = Article.all
+    @articles = Article.all
   end
 
   def show
@@ -13,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-  @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def create
